48fc088d33c6de24d9e48d837c70091e56037434
Noah Trivedi <noahtrivedi@gmail.com>
2018-09-14 21:01:40 -0400

    Remove deprecated type change from string constant to char pointer

--- pi-gen.cpp
+++ pi-gen.cpp
@@ -1,15 +1,14 @@
 //pi-gen.cpp: a program that will generate pi to the xth decimal place
+#include <cmath>
 #include <iostream>
 using namespace std;
 
-char * pi = "3.14159265358979323846264";
 
 int main() {
+    const double pi = 3.141592653589793;
     int places;
-    cout << "Enter a number from 1 to 23, and I will print out pi to that decimal place (not rounded): ";
+    cout << "Enter a number from 1 to 15, and I will print out pi to that decimal place (not rounded): ";
     cin >> places;
-    for (int i = 0; i < places + 2; i++) {
-        cout << pi[i];
-    }
-    cout << "\n";
+    cout.precision(places + 1);
+    cout << pi << "\n";
 }
